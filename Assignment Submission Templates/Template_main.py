1c5e97e7c0521eaf89b792c36bd9ea975756e640
Inigo Lopez-Gazpio <inigo.lopezgazpio@deusto.es>
2021-04-17 16:55:24 +0200

    Add assignment submission templates

---
+++ Assignment Submission Templates/Template_main.py
@@ -0,0 +1,36 @@
+# =======================================================================
+# Names:
+# Group Number:
+# Assignment:
+# Date:
+# =======================================================================
+# 1. Rename this file as: A[Number]_[Group].py
+# 2. Be sure to include, with this template, any necessary files for execution, including datasets
+# 3. If you use a function of an external package, do not forget to include the corresponding library import
+# 4. Do not forget to comment on the code, especially those non-trivial commands (remember that part of the rating depends on the cleaning of the code)
+# 5. It is strongly recommended to test any implemented function in order to check for its proper operation
+# =======================================================================
+
+
+
+
+# =======================================================================
+# Parameters for the Environment
+
+
+
+
+# =======================================================================
+# LIBRARIES (add any needed library here)
+
+
+
+
+# =======================================================================
+# Code of the assignement
+
+
+
+
+# =======================================================================
+# Tests
