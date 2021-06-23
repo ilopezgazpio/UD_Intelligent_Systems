277fc82848a7faee261239cf8e86c616aa6686d4
Inigo Lopez Gazpio <inigo.lopezgazpio@deusto.es>
2021-06-23 17:50:43 +0200

    Add larger episode limit

--- OpenAIGym_extra_environments/gym-8Puzzle/gym_8Puzzle/__init__.py
+++ OpenAIGym_extra_environments/gym-8Puzzle/gym_8Puzzle/__init__.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 register(
     id='8Puzzle-v0',
     entry_point='gym_8Puzzle.envs:PuzzleEnv',
-    max_episode_steps=1000000,
+    max_episode_steps=1000000000,
     reward_threshold=1.0,
     nondeterministic = False,
 )
--- OpenAIGym_extra_environments/gym-RiverCrossing/gym_RiverCrossing/__init__.py
+++ OpenAIGym_extra_environments/gym-RiverCrossing/gym_RiverCrossing/__init__.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 register(
     id='RiverCrossing-v0',
     entry_point='gym_RiverCrossing.envs:RiverCrossingEnv',
-    max_episode_steps=1000,
+    max_episode_steps=1000000000,
     reward_threshold=1.0,
-    nondeterministic = False,
+    nondeterministic= False,
 )
