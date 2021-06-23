a8f60e13fb67e9b8db0ee8fa05a0618a1626d2e8
Inigo Lopez Gazpio <inigo.lopezgazpio@deusto.es>
2021-06-23 13:29:14 +0200

    Agent as ancestor of all concrete agents - beta version

--- UDAI/agent/Agent.py
+++ UDAI/agent/Agent.py
@@ -1,30 +1,94 @@
 from abc import ABC, abstractmethod
 from gym import spaces
 from copy import deepcopy
-from ..frontier.Node import Node
+from UDAI.frontier.Node import Node
+from UDAI.frontier.Frontier import Frontier
+from UDAI.reporting.Report import Report
 
 class Agent(ABC):
 
-    ''' notes
-     No argument must be mandatory
-     Receive in constructor:
-     initial observation
-     Reference of environment
-    '''
+    # TODO iteration count limiter
+
+    def __init__(self):
+        self.step = 0
+        self.initial_node = None
+        self.final_node = None
+        self.frontier = Frontier()
+        self.reporting = Report()
 
 
-    # iteration count limiter
+    def __initialize_frontier__(self, initial_node : Node):
+        self.initial_node = initial_node
+        self.frontier.__insert_one_left__(initial_node)
 
-    # History logging per iteration and plotting
 
-    def __heuristic__(self, node: Node):
-        ''' Function to get the utility value of a node '''
+    def __printlog__(self, print_every=100):
+        if (self.step % print_every == 0):
+            print(self.reporting.log.tail(1))
+
+
+    def __blind_search__(self, max_steps=5000):
+        '''
+        This method implements the main search loop and must be implemented with the particularities of each search agent
+        '''
+        # Dummy node for initialization, done = false
+        current = Node()
+
+        while not current.done and len(self.frontier.nodes) > 0 and self.step <= max_steps:
+            current = self.frontier.__get_one_left__()
+            expanded = self.__expand_node__(current)
+
+            # Algorithm agnostic implementation
+            self.frontier.frontier_insertion_function(expanded)
+
+            self.reporting.__append__(current.done, current.cost, len(expanded),
+                                      self.frontier.__get_frontier_max_depth__(), len(self.frontier.nodes),
+                                      current.depth, current.cost)
+            self.__printlog__(print_every=1)
+
+            self.step += 1
+
+        if current.done:
+            self.final_node = current
+
+
+    def __save_iteration__(self):
+        pass
+
+
+    def __heuristic__(self, node: Node) -> float:
+        '''
+
+        Parameters
+        ----------
+        node
+
+        Returns
+        -------
+        Utility value of a node (from the standpoint of the agent)
+
+        '''
+
         return 0
 
-    def __expand_node__(self, node: Node):
+
+    def __expand_node__(self, node: Node) -> list:
         '''
+
+        Parameters
+        ----------
+        node
+
+        Returns
+        -------
+        a list of Nodes consequence of expanding node
+
+        Note
+        -----
         action_space must be an openai gym Space accepted by environment.step(action) for every action in action_space
         Note that not all openai gym environments implement the __is_applicable__ internal method.
+        If so __expand_node__ should be overwritten accordingly :)
+
         '''
 
         action_space = node.env.action_space
