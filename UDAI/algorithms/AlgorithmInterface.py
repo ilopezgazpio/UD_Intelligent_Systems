a941669fc12bb2034266a179d17ae3cfc6915e91
Inigo Lopez Gazpio <inigo.lopezgazpio@deusto.es>
2021-06-23 13:27:20 +0200

    deleted files

--- UDAI/algorithms/AlgorithmInterface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from ..frontier.Frontier import Frontier
-
-class AlgorithmInterface(ABC):
-
-    @abstractmethod
-    def sort(self, frontier: Frontier) -> Frontier:
-        None
-
